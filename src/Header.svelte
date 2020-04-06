157155f942cb936fd03240904f71d6897762e045
Robin Lack <lack@puzzle.ch>
2020-04-06 17:21:18 +0200

    add links to header

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,5 +1,33 @@
 <div class="row header">
 <div class="col s12 valign center light-blue darken-1">
-    <h3>BAG COVID-19 Data Visualization</h3>
+    <h3>
+        BAG COVID-19 Data Visualization
+        <a href="https://twitter.com/lack_robin/"><span class="valign right icon icon-twitter"></span></a>
+        <a href="https://github.com/lackrobin/bag-covid-visuals"><span class="valign right icon icon-github"></span></a>
+    </h3>
 </div>
+
 </div>
+
+<style>
+.icon-twitter {
+  mask: url(/twitter.svg);
+}
+.icon-github {
+  mask: url(/github.svg);
+}
+.icon {
+  display: inline-block;
+  margin: 10px;
+  display: flex;
+  align-items: center;
+  width: 50px;
+  height: 50px;
+  background: black;
+  mask-size: cover;
+}
+.icon:hover,
+.icon:focus {
+  background: rgb(47, 47, 47);
+}
+</style>
